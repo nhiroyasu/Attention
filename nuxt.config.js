ec2efc0a421770f901cae32d24ff84da6c77f64b
Cyan-on-win <namid224y@gmail.com>
2020-10-24 21:23:53 +0900

    first setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,9 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: [
+    '~/assets/fonts.scss'
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
