b4d0bae86f081f99e2a5a83f09b7b654edde762a
cyan <namid224y@gmail.com>
2020-10-26 10:20:03 +0900

    最低限の形にはした

---
+++ plugins/mapping/code-type.js
@@ -0,0 +1,45 @@
+export const options = [
+  {
+    value: 'hl',
+    text: 'HTML',
+  },
+  {
+    value: 'cs',
+    text: 'CSS',
+  },
+  {
+    value: 'js',
+    text: 'JavaScript',
+  },
+  {
+    value: 'py',
+    text: 'Python',
+  },
+  {
+    value: 'c',
+    text: 'C',
+  },
+  {
+    value: 'jv',
+    text: 'Java',
+  },
+  {
+    value: 'sw',
+    text: 'Swift',
+  },
+  {
+    value: 'kt',
+    text: 'Kotlin',
+  },
+];
+
+export const modeFiles = {
+  ht: '',
+  cs: '',
+  js: '',
+  py: '',
+  c: '',
+  jv: '',
+  sw: '',
+  kt: '',
+};
