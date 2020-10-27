831cedd7ac837095412edecd86b4f9600edda816
cyan <namid224y@gmail.com>
2020-10-27 09:49:54 +0900

    storeの管理

--- store/pickup.js
+++ store/pickup.js
@@ -1,5 +1,13 @@
 export const state = () => ({
   code: '',
+  position: {
+    anchor: {
+      line: 0,
+    },
+    head: {
+      line: 0,
+    },
+  },
 });
 
 export const getters = {
@@ -9,10 +17,21 @@ export const getters = {
   getShowing: (state) => {
     return !!state.code;
   },
+  getHeadLine: (state) => {
+    const lineNum = state.position.head.line;
+    return lineNum || lineNum === 0 ? lineNum + 1 : 0;
+  },
+  getAnchorLine: (state) => {
+    const lineNum = state.position.anchor.line;
+    return lineNum || lineNum === 0 ? lineNum + 1 : 0;
+  },
 };
 
 export const mutations = {
   updateCode: (state, payload) => {
     state.code = payload || '';
+  },
+  updatePosition: (state, payload) => {
+    state.position = payload;
   },
 };
